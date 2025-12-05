85704d32ade6ca5f444be2f6fcb1ebc69c6617ee
Vivi-tran <nguyen-ngoc-vi.tran.6279@student.uu.se>
2025-12-05 17:28:01 +0100

    MODIFY: modify output tar.gz

--- native/download.py
+++ native/download.py
@@ -5,6 +5,7 @@ import urllib.error
 import argparse
 from typing import Optional
 import pandas as pd
+import tarfile
 
 
 def download(pdb_id: str, out_dir: str | Path = ".") -> Path:
@@ -114,8 +115,13 @@ def main():
     output_dir = Path(args.output_dir) / "natives"
     retrieve_natives(args.input, output_dir)
     shutil.copy2(args.input, output_dir / Path(args.input).name)
+    
+    # Create tar.gz archive
+    archive_name = output_dir.parent / f"{output_dir.name}.tar.gz"
+    with tarfile.open(archive_name, "w:gz") as tar:
+        tar.add(output_dir, arcname=output_dir.name)
+    shutil.rmtree(output_dir)
     return
-
 
 if __name__ == "__main__":
     main()
